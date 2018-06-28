f3efe3f57ffa9667d05d1927245666e685d566bc
Douglas Lowder <dlowder@salesforce.com>
2018-06-28 11:41:36 -0700

    Different test image for TVPosterView

--- js/App.js
+++ js/App.js
@@ -81,7 +81,7 @@ class App extends Component {
               <TVPosterView
                 title="TVPosterView"
                 subtitle="A cool view"
-                imageURL='flowers'
+                imageURL='like'
               />
             </View>
           </Slide>
